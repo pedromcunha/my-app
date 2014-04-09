1e9fd979a5e21d695c34e92afa673636f038cb18
Pedro Cunha <pedro.spinsomewebs@gmail.com>
2014-04-09 16:00:14 -0400

    Add scraper program

--- angular/Node scraper/scrape.js
+++ angular/Node scraper/scrape.js
@@ -1,3 +1,25 @@
 var request = require('request');
 var cheerio = require('cheerio');
+//Now ask to be served the webpage
+request('http://students.startupinstitute.com/2014/new-york/spring/', function (error, response, html) {
+	//Great check to see if theres any errors and print html
+
+	if (!error && response.statusCode == 200) {
+		var $ = cheerio.load(html);
+		var dataArray = [];
+		//Load that into a variable called money. Loop it so that it parses the data for a label
+		$('p.lead').each(function(i, element) {
+			var a = $(this);
+			var name = a.text();
+			var imageUrl = a.prev().children().attr('src');
+			var scrapeData = {
+				name: name,
+				image: imageUrl
+			};
+			dataArray.push(scrapeData);
+			//Last but not least push it into an array we created earlier.
+			console.log(dataArray);
+		});
+	}
+});
 
